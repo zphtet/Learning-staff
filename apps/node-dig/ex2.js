f59b30c7da7498ddd5afee58eed1af3d27e65746
zphtet <zinphtet.215108@gmail.com>
2024-05-11 23:01:20 +0630

    transform stream to uppercase

--- apps/node-dig/ex2.js
+++ apps/node-dig/ex2.js
@@ -7,6 +7,7 @@ const path = require("path");
 const fs = require("fs");
 const { error } = require("console");
 const getStdin = require("get-stdin");
+const Transform = require("stream").Transform;
 // require("dotenv").config();
 const args = require("minimist")(process.argv.slice(2), {
   boolean: ["watch", "in"],
@@ -32,6 +33,17 @@ if (args.in) {
   processStream(process.stdin);
 }
 
-function processStream(inputStream) {
-  inputStream.pipe(process.stdout);
+function processStream(inStream) {
+  let inputStream = inStream;
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(chunk.toString().toUpperCase());
+      setTimeout(callback, 1000);
+      // callback();
+    },
+  });
+
+  inputStream = inputStream.pipe(transformStream);
+  const outputStream = process.stdout;
+  inputStream.pipe(outputStream);
 }

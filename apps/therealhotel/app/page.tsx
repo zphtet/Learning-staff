383e54cc0f0b03e381e8d8194b701d08ca22d681
zphtet <zinphtet.215108@gmail.com>
2024-11-19 16:50:29 +0630

    stay in know sec

--- apps/therealhotel/app/page.tsx
+++ apps/therealhotel/app/page.tsx
@@ -2,6 +2,7 @@ import Banner from "@/components/banner";
 import ExploreMap from "@/components/explore-map";
 import InTheWild from "@/components/in-the-wild";
 import Stacks from "@/components/stacks";
+import StayInTheKnow from "@/components/stay-in-known";
 export default function Home() {
   return (
     <div className="">
@@ -11,7 +12,13 @@ export default function Home() {
         <InTheWild text="In The Wild" />
       </div>
       <Stacks />
-      <div className="h-[100vh]"></div>
+      <div className="py-36">
+        <StayInTheKnow text="Stay In The Know" />
+        <p className="text-center">
+          Be the first to know about new hotels we’ve uncovered
+        </p>
+      </div>
+      {/* <div className="h-[100vh]"></div> */}
     </div>
   );
 }

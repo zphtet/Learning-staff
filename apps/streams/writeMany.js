5678176a6f9afa9bd3f0688253faea415b6bf81e
zphtet <zinphtet.215108@gmail.com>
2025-03-15 09:54:06 +0630

    fix write memory issue by using writeable stream

--- apps/streams/writeMany.js
+++ apps/streams/writeMany.js
@@ -15,20 +15,95 @@
 //   console.timeEnd("writeMany");
 // })();
 
-import fs from "node:fs";
+// import fs from "node:fs";
 
 // 1.8 s   77 % CUP    130 MB
+// (async () => {
+//   fs.open("text.txt", "w", (err, fd) => {
+//     console.time("writeMany");
+//     for (let i = 0; i < 1000000; i++) {
+//       fs.writeSync(fd, ` ${i} `, () => {});
+//     }
+//     console.timeEnd("writeMany");
+//   });
+//   //   console.time("writeMany");
+//   //   for (let i = 0; i < 1000000; i++) {
+//   //     await fileHandler.write(` ${i} `);
+//   //   }
+//   //   console.timeEnd("writeMany");
+// })();
+
+const fs = require("node:fs/promises");
+
+// (async () => {
+//   const fileHandler = await fs.open("text.txt", "w");
+
+//   const stream = fileHandler.createWriteStream();
+
+//   console.log(stream.writableHighWaterMark);
+//   console.log(stream.writableLength);
+//   // const buff = Buffer.from("string");
+//   // stream.write(buff);
+//   // stream.write(buff);
+//   // stream.write(buff);
+//   // stream.write(buff);
+
+//   const buff = Buffer.alloc(65536, "a");
+//   const chunkState = stream.write(buff);
+//   console.log("Chunk state", chunkState);
+
+//   console.log("Writeable length", stream.writableLength);
+
+//   stream.on("drain", () => {
+//     console.log("Drained successfully  : Save to write buffer");
+//     stream.write(Buffer.alloc(6550, "b"));
+//     console.log("Writeable length", stream.writableLength);
+//   });
+
+//   // fileHandler.close();
+// })();
+
 (async () => {
-  fs.open("text.txt", "w", (err, fd) => {
-    console.time("writeMany");
-    for (let i = 0; i < 1000000; i++) {
-      fs.write(fd, ` ${i} `, () => {});
+  const fileHandler = await fs.open("text.txt", "w");
+
+  const stream = fileHandler.createWriteStream();
+
+  let i = 0;
+
+  console.time("WriteMany");
+  const writeMany = () => {
+    let canWrite = true;
+    while (i < 1000000 && canWrite) {
+      const buff = Buffer.from(` ${i} `, "utf-8");
+
+      canWrite = stream.write(buff);
+      // console.log("Can write", canWrite);
+      if (i === 1000000 - 1) {
+        stream.end();
+
+        console.log("End : Write all numbers");
+        return;
+      }
+      i++;
     }
-    console.timeEnd("writeMany");
+
+    // if (i < 1000000) {
+    //   stream.once("drain", writeMany);
+    // }
+
+    // fileHandler.close();
+  };
+
+  writeMany();
+
+  stream.on("drain", () => {
+    writeMany();
   });
-  //   console.time("writeMany");
-  //   for (let i = 0; i < 1000000; i++) {
-  //     await fileHandler.write(` ${i} `);
-  //   }
-  //   console.timeEnd("writeMany");
+
+  stream.on("finish", () => {
+    console.timeEnd("WriteMany");
+    fileHandler.close();
+  });
+
+  // fileHandler.close();
 })();

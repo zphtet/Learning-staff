e3c7e395c2d39d43fcd68b25ab13b2b429319f4f
zphtet <zinphtet.215108@gmail.com>
2025-04-08 22:58:09 +0630

    copy file

---
+++ apps/node-concepts/streams/copy/copy.js
@@ -0,0 +1,39 @@
+import fs from "node:fs/promises";
+
+// file size 1GB , time : 900ms  : space : 1GB
+// (async () => {
+//   console.time("copy");
+//   const destFile = await fs.open("dest.txt", "w");
+//   const srcFileResult = await fs.readFile("text.txt");
+
+//   await destFile.write(srcFileResult);
+//   console.timeEnd("copy");
+// })();
+
+(async () => {
+  console.time("copy");
+
+  const srcFile = await fs.open("text.txt", "r");
+  const destFile = await fs.open("text-copy.txt", "w");
+
+  //   console.log(await srcFile.read());
+  //   console.log(await srcFile.read());
+
+  let bytesRead = -1;
+
+  while (bytesRead !== 0) {
+    const readResult = await srcFile.read();
+    bytesRead = readResult.bytesRead;
+
+    if (bytesRead !== 16384) {
+      const indexOfNotFilled = readResult.buffer.indexOf(0);
+      await destFile.write(readResult.buffer.slice(0, indexOfNotFilled));
+    } else {
+      destFile.write(readResult.buffer);
+    }
+  }
+
+  console.timeEnd("copy");
+  await srcFile.close();
+  await destFile.close();
+})();

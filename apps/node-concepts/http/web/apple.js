9bdc5889fd048dbe3824f32c86b779c7f695d87e
zphtet <zinphtet.215108@gmail.com>
2025-05-23 14:01:29 +0630

    register route func

--- apps/node-concepts/http/web/apple.js
+++ apps/node-concepts/http/web/apple.js
@@ -6,48 +6,35 @@ async function sendFile(filePath, type, res) {
   const fileStream = await fileHandle.createReadStream();
   res.setHeader("Content-Type", type);
   fileStream.pipe(res);
-//   fileHandle.close();
+  //   fileHandle.close();
 }
+
 class Butter {
   constructor() {
     this.server = http.createServer();
+
+    this.routes = {};
+
     this.server.on("request", (req, res) => {
       console.log("Request Come in", req.url, req.method);
+      res.sendFile = (filePath, type) => sendFile(filePath, type, res);
+      res.status = function (code) {
+        res.statusCode = code;
+        return this;
+      };
+
+      if (this.routes[req.method + req.url]) {
+        this.routes[req.method + req.url](req, res);
+      } else {
+        res.status(404).sendFile("./public/404.html", "text/html");
+      }
+
+      // this.routes[req.method + req.url](req, res);
     });
   }
 
   route(method, path, cb) {
-    this.server.on("request", (req, res) => {
-      if (method === "GET") {
-        res.status = function (code) {
-          res.statusCode === code;
-          return this;
-        };
-        res.sendFile = (filePath, type) => sendFile(filePath, type, res);
-
-        // if (
-        //   req.url !== "/" &&
-        //   (req.url.endsWith(".css") || req.url.endsWith(".js"))
-        // ) {
-        //   const filePath = `./public/${req.url.split("/")[1]}`;
-        //   const type =
-        //     (req.url.endsWith(".css") && "text/css") ||
-        //     (req.url.endsWith(".js") && "text/javascript");
-        //   sendFile(req.url.split(filePath), type, res);
-        // }
-
-        if (req.url === "/style.css") {
-          console.log("Requesting css");
-          sendFile("./public/style.css", "text/css", res);
-        }
-
-        if (req.url === "/app.js") {
-          sendFile("./public/app.js", "text/javascript", res);
-        }
-
-        cb(req, res);
-      }
-    });
+    this.routes[method + path] = cb;
   }
 
   listen(port, cb) {
--- apps/node-concepts/http/web/server.js
+++ apps/node-concepts/http/web/server.js
@@ -6,6 +6,14 @@ app.route("GET", "/", (req, res) => {
   res.status(200).sendFile("./public/index.html", "text/html");
 });
 
+app.route("GET", "/style.css", (req, res) => {
+  res.status(200).sendFile("./public/style.css", "text/css");
+});
+
+app.route("GET", "/app.js", (req, res) => {
+  res.status(200).sendFile("./public/app.js", "text/javascript");
+});
+
 // app.route("GET", "/", (req, res) => {
 //   res.status(200).sendFile("./public/style.css", "text/css");
 // });

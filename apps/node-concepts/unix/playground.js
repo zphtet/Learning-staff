9d5262b89a0bf8433e867c120cf4c7c4227386dd
zphtet <zinphtet.215108@gmail.com>
2025-08-20 21:12:41 +0630

    running subprocess

--- apps/node-concepts/unix/playground.js
+++ apps/node-concepts/unix/playground.js
@@ -3,7 +3,7 @@
 const {spawn , exec} = require('node:child_process')
 
 
-const subprocess =  spawn('zsh' ,["./scripts.sh"])
+const subprocess =  spawn('/Applications/Telegram.app/Contents/MacOS/Telegram')
 
 subprocess.stdout.on('data',(data)=>{
   console.log(data.toString('utf-8'))
--- apps/node-concepts/unix/scripts.sh
+++ apps/node-concepts/unix/scripts.sh
@@ -2,5 +2,7 @@ x=100
 y=50
 # sleep 4
 
+z=200
+
 echo total is $((x+y))
 # sleep 4
